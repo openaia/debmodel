b30fd75d3397a14c065c6620efcbbd608f0b9c38
Jagan Teki <jagan@edgeble.ai>
2024-05-22 21:02:02 +0530

    download-artifacts: use openaia for model-zoo
    Signed-off-by: Jagan Teki <jagan@edgeble.ai>

--- download-artifacts.sh
+++ download-artifacts.sh
@@ -13,7 +13,7 @@ wget -qP prebuilt "https://github.com/openaia/u-boot/releases/download/$U_BOOT_R
 wget -qP prebuilt "https://github.com/openaia/u-boot/releases/download/$U_BOOT_REL/u-boot.itb"
 
 echo "=== Downloading model-zoo ==="
-cd prebuilt &&  git clone https://github.com/edgeble/model-zoo.git && cd -
+cd prebuilt &&  git clone https://github.com/openaia/model-zoo.git && cd -
 
 find prebuilt/
 echo "=== Remember to remove prebuilt directory for new aritfacts to load ==="
